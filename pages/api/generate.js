d512360f454290eaf021ccf74937e37441a665d9
wheelerworks <106202945+wheelerworks@users.noreply.github.com>
2022-05-29 12:34:37 -0400

    commit on pr/429*

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,7 +19,7 @@ function generatePrompt(animal) {
   return `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Names: Captain SHARPCLAW, Agent Fluffball, The Incredible Feline
 Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: ${capitalizedAnimal}
